1d59c2cf561b60c99fd923fa97ddb5474ec6080b
Rayllanderson <rayllanderson@gmail.com>
2021-05-16 18:53:15 -0300

    Variables & Data Types

--- main.dart
+++ main.dart
@@ -1,3 +1,17 @@
 void main (){
-  print('Hello World!');
+  // Variables
+  String characterName = 'Ray';
+  int characterAge = 70;
+
+  print('There once was a man named ${characterName}');
+  print('He was ${characterAge} years old');
+  print('He really liked the name ${characterName}');
+  print("But he didn't like being ${characterAge}");
+
+  //Data Types
+  String firstName = "João";
+  int age = 24;
+  double weight = 95.5;
+  bool isAbove18 = true;
+  print('${firstName}, ${age}, ${weight}KG.\nAbove 18? ${isAbove18}');
 }
